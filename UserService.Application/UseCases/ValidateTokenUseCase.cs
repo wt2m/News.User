99f7a9abbcd96acd38347317149cfbf9999d3113
Miguel Alves <52261424+wt2m@users.noreply.github.com>
2024-10-16 18:44:58 -0300

    [FEAT] Token Validation

---
+++ UserService.Application/UseCases/ValidateTokenUseCase.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserService.Application.DTOs;
+using UserService.Domain.Entities;
+using UserService.Infrastructure.Identity;
+
+namespace UserService.Application.UseCases
+{
+    public class ValidateTokenUseCase
+    {
+        private readonly JwtTokenService _jwtTokenService;
+
+        public ValidateTokenUseCase(JwtTokenService jwtTokenService)
+        {
+            _jwtTokenService = jwtTokenService;
+        }
+
+        public bool ValidateUserToken(string token)
+        {
+            var result = _jwtTokenService.VerifyTokenAsync(token);
+
+            if (result == null)
+                return false;
+
+            return true;
+        }
+    }
+}
