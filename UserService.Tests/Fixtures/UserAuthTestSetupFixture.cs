024a2c56c7e0580a6e549cc9109a179e02d3f455
Miguel Alves <52261424+wt2m@users.noreply.github.com>
2024-11-05 18:51:30 -0300

    [FEAT] Corrigindo unit tests

--- UserService.Tests/Fixtures/UserAuthTestSetupFixture.cs
+++ UserService.Tests/Fixtures/UserAuthTestSetupFixture.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Moq;
 using UserService.Application.Interfaces;
 using UserService.Domain.Entities;
@@ -9,12 +14,13 @@ using UserService.Infrastructure.DependencyInjection;
 
 namespace UserService.Tests.Fixtures
 {
-    public class UserAuthTestSetupFixture : IAsyncLifetime
+    public class UserAuthTestSetupFixture : IDisposable
     {
         public ServiceProvider ServiceProvider { get; private set; }
         public Mock<IUnitOfWork> UnitOfWorkMock { get; private set; }
         public Mock<IUserRepository> UserRepositoryMock { get; private set; }
         public Mock<ILogService> LoggerMock { get; private set; }
+        public Mock<IHttpContextAccessor> HttpContextMock { get; private set; }
 
         public UserAuthTestSetupFixture()
         {
@@ -45,6 +51,12 @@ namespace UserService.Tests.Fixtures
             services.AddInfrastructureServices();
             services.AddSingleton(configuration);
             services.AddLogging();
+
+            // Add required dependencies for SignInManager
+            services.AddSingleton<IOptions<IdentityOptions>, OptionsManager<IdentityOptions>>();
+            services.AddSingleton<ILogger<SignInManager<User>>, Logger<SignInManager<User>>>();
+            services.AddSingleton<IAuthenticationSchemeProvider, AuthenticationSchemeProvider>();
+
             return services;
         }
 
@@ -63,21 +75,29 @@ namespace UserService.Tests.Fixtures
             UnitOfWorkMock.Setup(uow => uow.Users).Returns(UserRepositoryMock.Object);
             UnitOfWorkMock.Setup(uow => uow.CompleteAsync()).ReturnsAsync(1);
 
+            // HttpContext mocks
+            var httpContext = new DefaultHttpContext();
+            HttpContextMock = new Mock<IHttpContextAccessor>();
+            HttpContextMock.Setup(_ => _.HttpContext).Returns(httpContext);
+
+            // Mock AuthenticationService for SignInManager
+            var authenticationServiceMock = new Mock<IAuthenticationService>();
+            services.AddSingleton(authenticationServiceMock.Object);
+
             services.AddSingleton(UnitOfWorkMock.Object);
             services.AddSingleton(LoggerMock.Object);
+            services.AddSingleton(HttpContextMock.Object);
         }
 
-        public Task InitializeAsync()
+        public void Dispose()
         {
-            // Any async initialization can go here if necessary.
-            return Task.CompletedTask;
-        }
-
-        public async Task DisposeAsync()
-        {
-            if (ServiceProvider != null)
+            if (ServiceProvider is IAsyncDisposable asyncDisposable)
             {
-                await ServiceProvider.DisposeAsync();
+                asyncDisposable.DisposeAsync().GetAwaiter().GetResult();
+            }
+            else
+            {
+                ServiceProvider.Dispose();
             }
         }
     }
